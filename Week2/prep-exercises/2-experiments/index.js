77ece52ceee31311311769232fb3cadc18fdf702
Hanna <141053475+HannaInIT@users.noreply.github.com>
2025-02-22 22:54:46 +0100

    Update index.js

--- Week2/prep-exercises/2-experiments/index.js
+++ Week2/prep-exercises/2-experiments/index.js
@@ -1,20 +1,34 @@
 "use strict";
 
 function runExperiment(sampleSize) {
-  const valueCounts = [0, 0, 0, 0, 0, 0];
+    const valueCounts = [0, 0, 0, 0, 0, 0];
+    const results = []; 
 
-  // TODO
-  // Write a for loop that iterates `sampleSize` times (sampleSize is a number).
-  // In each loop iteration:
-  //
-  // 1. Generate a random integer between 1 and 6 (as if throwing a six-sided die).
-  // 2. Add `1` to the element of the `valueCount` that corresponds to the random
-  //    value from the previous step. Use the first element of `valueCounts`
-  //    for keeping a count how many times the value 1 is thrown, the second
-  //    element for value 2, etc.
+    // TODO
+    // Write a for loop that iterates `sampleSize` times (sampleSize is a number).
+    // In each loop iteration:
+    //
+    // 1. Generate a random integer between 1 and 6 (as if throwing a six-sided die).
+    // 2. Add `1` to the element of the `valueCount` that corresponds to the random
+    //    value from the previous step. Use the first element of `valueCounts`
+    //    for keeping a count how many times the value 1 is thrown, the second
+    //    element for value 2, etc.
+    
+    for (let i = 0; i < sampleSize; i++){
+        const roll = Math.floor(Math.random() * 6) + 1;
+        valueCounts[roll - 1]++;
+      
+    }
 
-  const results = [];
+    for (let count of valueCounts) {
+        const percentage = ((count / sampleSize) * 100).toFixed(2); 
+        results.push(percentage);  
+    }
+ 
+    return results;
+        
 
+ 
   // TODO
   // Write a for..of loop for the `valueCounts` array created in the previous
   // loop. In each loop iteration:
@@ -25,7 +39,6 @@ function runExperiment(sampleSize) {
   //    two decimals, e.g. '14.60'.
   // 3. Then push that string onto the `results` array.
 
-  return results;
 }
 
 function main() {
@@ -40,7 +53,12 @@ function main() {
   //
   // [ '26.00', '17.00', '10.00', '19.00', '16.00', '12.00' ] 100
   // [ '14.60', '17.10', '19.30', '15.50', '16.70', '16.80' ] 1000
-  // [ '16.71', '16.68', '16.69', '16.66', '16.67', '16.59' ] 1000000
+    // [ '16.71', '16.68', '16.69', '16.66', '16.67', '16.59' ] 1000000
+    
+    for (let sampleSize of sampleSizes) {
+        const result = runExperiment(sampleSize); 
+        console.log(`Results for sample size ${sampleSize}:`, result);  
+    }
 }
 
 main();
